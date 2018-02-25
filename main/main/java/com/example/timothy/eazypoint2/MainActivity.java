5b1c06661ba79ef6a43249b9bf8598f8abae6d37
FantasyWings <36182014+FantasyWings@users.noreply.github.com>
2018-02-25 17:11:35 +0800

    EazyPoint2
    0.0.0

---
+++ main/main/java/com/example/timothy/eazypoint2/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.timothy.eazypoint2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home){
+
+            Intent Nhome = new Intent(MainActivity.this, MapActivity.class);
+            startActivity(Nhome);
+
+        } else if (id == R.id.nav_location) {
+
+            Intent Nlocation = new Intent(MainActivity.this, LocationActivity.class);
+            startActivity(Nlocation);
+
+        } else if (id == R.id.nav_forder) {
+
+            Intent Nforder = new Intent(MainActivity.this, ForderActivity.class);
+            startActivity(Nforder);
+
+        } else if (id == R.id.nav_friend) {
+
+            Intent Nfriend = new Intent(MainActivity.this, FriendActivity.class);
+            startActivity(Nfriend);
+
+        } else if (id == R.id.nav_share) {
+
+            Intent Nshare = new Intent(MainActivity.this, ShareActivity.class);
+            startActivity(Nshare);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
